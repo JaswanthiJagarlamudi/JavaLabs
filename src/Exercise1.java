fdda62e4c2c4466f12cbf3776f76467ef2473db3
JaswanthiJagarlamudi <80451064+JaswanthiJagarlamudi@users.noreply.github.com>
2021-03-11 18:27:06 +0530

    Lab1

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -1,24 +1,22 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Exercise1 {
 	
-	public static int getSecondSmallest(int a[]) {
-		Arrays.sort(a);
-		return a[1];
-	}
-
+	
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter the number of elements");
-		int N=sc.nextInt();
-		int a[]=new int[N];
-		System.out.println("Enter the integers of the array");
-		for(int i=0;i<N;i++) {
-			a[i]=sc.nextInt();
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the number");
+		int n=s.nextInt();
+		int sum=0,a;
+		while(n!=0)
+		{
+		a = n%10;
+		sum = sum +(a*a*a);
+		n = n/10;
+
 		}
-    
-	System.out.println("Second Smallest Element:"+getSecondSmallest(a));
+		System.out.println(sum);
+
 	}
 
 }
--- src/Exercise4.java
+++ src/Exercise4.java
@@ -1,50 +1,23 @@
-import java.util.*;
-
+import java.util.Scanner;
 
 public class Exercise4 {
-	public static int modifyArray(int arr[], int N){  
-        if (N==0 || N==1){  
-            return N;  
-        }  
-        int[] temp = new int[N];  
-        int j = 0;  
-        for (int i=0; i<N-1; i++){  
-            if (arr[i] != arr[i+1]){  
-                temp[j++] = arr[i];  
-            }  
-         }  
-        temp[j++] = arr[N-1];     
-        for (int i=0; i<j; i++){  
-            arr[i] = temp[i];  
-        }  
-        return j;  
-    }  
-	
+
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter the number of elements");
-		int N=sc.nextInt();
-		int[] a=new int[N];
-		System.out.println("Enter the integers of the array");
-		for(int i=0;i<N;i++) {
-			a[i]=sc.nextInt();
-		}
-		Arrays.sort(a);
-		
-		N = modifyArray(a, N);  
-        //printing array elements  
-        for (int i=0; i<N; i++)  
-           System.out.print(a[i]+" ");  
-     
-		
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the number");
+		int N=s.nextInt();
+		for(int i=1;i<=N;i++)
+        {
+            int p=0;
+            for(int j=1;j<=i;j++)
+            {
+                if(i%j==0)
+                    p++;
+            }
+            if(p==2)
+                    System.out.print(" "+i);
+ 
+        }
 	}
-	
- }
 
-	
-	
-         
-        
-	
-
-	
+}
